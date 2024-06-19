a3a74d926f0c2fc2bcf84f4a1abd7675a2d0e974
Hendra Lijaya <hendralijaya@gmail.com>
2024-06-19 14:45:38 +0700

    Initial Commit

---
+++ Emo/EmoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmoApp.swift
+//  Emo
+//
+//  Created by hendra on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct EmoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
